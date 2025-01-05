d32658571fcc7038c167dda7291b293fc93f9443
vivek <vivek.bnb@gmail.com>
2025-01-05 14:25:56 +0530

    refactor TabLayout: update tab icons for Transaction and Recurring tabs; remove unused modal code

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -48,7 +48,7 @@ export default function TabLayout() {
           options={{
             title: "Transaction",
             tabBarIcon: ({ color }) => (
-              <IconSymbol size={28} name="house.fill" color={color} />
+              <IconSymbol size={28} name="money.dollar" color={color} />
             ),
           }}
         />
@@ -57,7 +57,7 @@ export default function TabLayout() {
           options={{
             title: "Recurring",
             tabBarIcon: ({ color }) => (
-              <IconSymbol size={28} name="house.fill" color={color} />
+              <IconSymbol size={28} name="recurring.triangle" color={color} />
             ),
           }}
         />
@@ -71,51 +71,10 @@ export default function TabLayout() {
           }}
         />
       </Tabs>
-
-      {/* <Modal
-        transparent={true}
-        visible={isModalVisible}
-        animationType="slide"
-        presentationStyle="overFullScreen"
-        onDismiss={() => setIsModalVisible(false)}
-        onRequestClose={() => setIsModalVisible(false)}
-      >
-        <ThemedView style={styles.modalContainer}>
-        <AddTransactionComponent onClose={() => setIsModalVisible(false)} />
-        </ThemedView>
-        
-      </Modal> */}
-
       
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  
-  modalContainer: {
-    //flex: 1,
-    top: 100,
-    width: 300,
-    height: 300,
-    padding: 20,
-    borderRadius: 18,
-    margin: 20,
-    //justifyContent: "center",
-    alignItems: "center",
-    zIndex: 9999,
-    //backgroundColor: "rgba(0, 0, 0, 0.5)", // semi-transparent background
-  },
-  modalBackdrop: {
-    position: "fixed",
-    top: "50%",
-    left: "50%",
-    transform: [{ translateX: "-50%" }, { translateY: "-50%" }],
-    width: 300,
-    height: 700,
-    zIndex: 9999, // Ensure it's above other content
-    //backgroundColor: "rgba(236, 18, 18, 0.5)", // dark overlay
-    borderRadius: 18,
-  },
- 
 });
--- components/ui/IconSymbol.tsx
+++ components/ui/IconSymbol.tsx
@@ -13,6 +13,8 @@ const MAPPING = {
   'paperplane.fill': 'send',
   'chevron.left.forwardslash.chevron.right': 'code',
   'chevron.right': 'chevron-right',
+  'money.dollar': 'attach-money',
+  'recurring.triangle': 'repeat',
 } as Partial<
   Record<
     import('expo-symbols').SymbolViewProps['name'],
